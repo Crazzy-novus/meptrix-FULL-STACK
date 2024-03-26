58413755288db74d717eedfaf7419244c59129d5
Crazzy-novus <duraivignesh222@gmail.com>
2024-03-26 19:00:34 +0530

    Club insertion in backend completed

---
+++ meptrix-backend/routes/club.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createClub } from '../controllers/club.controller.js'; // Importing the controller for club
+
+const router = express.Router();  // Through router we can create get post methods for apis
+
+            /* For all CRUD operation we have creates the routes here and the logic for the api routes 
+                are written in controller folder. and we just call the controller here
+            */
+// Create a new club in DB
+router.post("/createclub", createClub );
+
+export default router; // Exporting the router so it could be used in server file
