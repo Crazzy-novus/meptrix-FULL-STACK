9dbae7c615fa309a39f8ff896ef07cb996be8e18
Santhosh-Sivan-Raja <santhoshsivan172003@gmail.com>
2024-03-30 19:19:24 +0530

    Event Validation

--- meptrix/src/app/component/contestcreation/contestcreation.component.ts
+++ meptrix/src/app/component/contestcreation/contestcreation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit, inject } from '@angular/core';
+
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contestcreation',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
   templateUrl: './contestcreation.component.html',
   styleUrl: './contestcreation.component.css'
 })
-export class ContestcreationComponent {
+export class ContestcreationComponent implements OnInit {
+  contentForm!: FormGroup;
 
+  fb = inject(FormBuilder);
+
+
+  ngOnInit(): void {
+    this.contentForm = this.fb.group({
+      contestname: ['', Validators.required],
+      clubevent: ['', Validators.required],
+      studentevent: ['', Validators.required],
+      contestvenue: ['', Validators.required],
+      contestdate: ['', Validators.required],
+      contesttime: ['', Validators.required],
+      contestshortdescripition: ['', [Validators.required, Validators.maxLength(100)]],
+      contestdescripition: ['', [Validators.required, Validators.maxLength(1000)]],
+      contestimg: ['', Validators.required], // You might need to adjust the validation for image upload
+    });
+  }
+
+  onSubmit() {
+    if (this.contentForm.valid) {
+      // Form is valid, you can submit it
+      console.log(this.contentForm.value);
+      this.contentForm.reset();
+      // Here you can call your service to send the form data to the server
+    } else {
+      // Form is invalid, handle errors or display messages to the user
+      console.log('Form is invalid');
+
+    }
+  }
 }
