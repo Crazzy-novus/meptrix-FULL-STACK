3184a03175ac221e76c25561924805ea4611c3a9
Santhosh-Sivan-Raja <santhoshsivan172003@gmail.com>
2024-04-09 00:43:38 +0530

    Color Code and some changes by Santhosh Sivan

--- meptrix/tailwind.config.js
+++ meptrix/tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         '96': '24rem',  // Default value
         'custom': '40rem', // Your custom value
       },
+      colors: {
+        // Replace with your desired color value (e.g., '#f0f2f5')
+        color1: '#440A67',
+        color2: '#93329E',
+        color3: '#B4AEF7',
+        color4: '#FFE3FE'
+      },
     },
   },
   plugins: [
