405f02ab7d2c3d9561a5d87b77996eb4587386a7
Santhosh-Sivan-Raja <santhoshsivan172003@gmail.com>
2024-04-18 16:02:24 +0530

    Home

---
+++ meptrix/src/app/component/eventregistration/eventregistration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventregistration',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventregistration.component.html',
+  styleUrl: './eventregistration.component.css'
+})
+export class EventregistrationComponent {
+
+}
